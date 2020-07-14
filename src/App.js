b5a2015c4f3dcf8682e59e0ae730e1fc3a369372
Glacius-A <vlad.skachovskiyy1@gmail.com>
2020-07-14 20:41:11 +0300

    03

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p> 
+        hello
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          ривітик Олюсь
         </p>
         <a
           className="App-link"
