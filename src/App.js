9d301b34147eae5cad67d0c6aae1a801de8df603
Glacius-A <vlad.skachovskiyy1@gmail.com>
2020-07-18 16:59:27 +0300

    progress1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import moviesData from '../src/moviesData';
 
-function App() {
-  return <div>hello</div>
+function removeMovie(movie) {
+  const updateMovies = this.state.movies.filter(function (item) {
+    return item.id !== movie.id
+  });
+  console.log(updateMovies);
+  this.setState({
+    movies: updateMovies
+  });
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: moviesData
+    };
+  }
+  render() {
+    console.log(this);
+    return (<div>{this.state.movies.map(movie => {
+      return <div>
+        <p>{movie.title}</p>
+        <button onClick={removeMovie.bind(this, movie)}> delet </button>
+      </div>
+    })
+    }
+    </div >
+    );
+  }
 }
+
+// function App() {
+//   return this.state.movies[1].title
+// }
 
 export default App;
